7a88f5b20cc02762135def2b15c74e665adb8a8c
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-09-19 20:30:48 -0500

    initial commit p2

---
+++ src/lab/polymorphism/TBExpt.java
@@ -0,0 +1,60 @@
+package lab.polymorphism;
+
+import java.io.PrintWriter;
+import org.w3c.dom.Text;
+
+/**
+ * A series of experiments with the text block layout classes.
+ * 
+ * @author Samuel A. Rebelsky
+ * @version 1.3 of September 2019
+ */
+public class TBExpt {
+  // +------+--------------------------------------------------------------
+  // | Main |
+  // +------+
+
+  public static void main(String[] args) throws Exception {
+    // Prepare for input and output
+    PrintWriter pen = new PrintWriter(System.out, true);
+
+    // Create a block to use
+    TextBlock block = new TextLine("This is a test.");
+
+    // Print out the block
+    TBUtils.print(pen, block);
+
+    TextBlock amazing = new TextLine("You are amazing.");
+
+    BoxedBlock awesome = new BoxedBlock(amazing);
+
+    TBUtils.print(pen, awesome);
+    
+    BoxedBlock incredible = new BoxedBlock(awesome);
+    TBUtils.print(pen, incredible);
+
+    TextBlock empty = new TextLine(" ");
+    BoxedBlock emptyBox = new BoxedBlock(empty);
+    TBUtils.print(pen, emptyBox);
+
+TextBlock helo = new TextLine("hello");
+TextBlock godbye = new TextLine("goodbye");
+BoxedBlock hello = new BoxedBlock(helo);
+BoxedBlock goodbye = new BoxedBlock(godbye);
+ VComposition hellogoodbye = new VComposition(hello, goodbye);
+
+ VComposition helogodbye = new VComposition(helo,godbye);
+
+ BoxedBlock helogodbyeblock = new BoxedBlock(helogodbye);
+ TBUtils.print(pen, helogodbyeblock);
+ TBUtils.print(pen,hellogoodbye);
+
+ HComposition comp1 = new HComposition(hello, godbye);
+ HComposition comp2 = new HComposition(godbye,hello);
+ TBUtils.print(pen,comp1);
+ TBUtils.print(pen,comp2);
+    // Clean up after ourselves.
+    pen.close();
+  } // main(String[])
+
+} // class TBExpt
