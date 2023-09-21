e4f7ca46ea0d4a490c28fb09b8dc9e2b52dde3fc
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-09-20 19:37:12 -0500

    truncated started

--- src/lab/polymorphism/TBExpt.java
+++ src/lab/polymorphism/TBExpt.java
@@ -18,43 +18,48 @@ public class TBExpt {
     // Prepare for input and output
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    // Create a block to use
-    TextBlock block = new TextLine("This is a test.");
+    TextBlock block = new TextLine("hello");
 
-    // Print out the block
-    TBUtils.print(pen, block);
-
-    TextBlock amazing = new TextLine("You are amazing.");
-
-    BoxedBlock awesome = new BoxedBlock(amazing);
-
-    TBUtils.print(pen, awesome);
-    
-    BoxedBlock incredible = new BoxedBlock(awesome);
-    TBUtils.print(pen, incredible);
-
-    TextBlock empty = new TextLine(" ");
-    BoxedBlock emptyBox = new BoxedBlock(empty);
-    TBUtils.print(pen, emptyBox);
-
-TextBlock helo = new TextLine("hello");
-TextBlock godbye = new TextLine("goodbye");
-BoxedBlock hello = new BoxedBlock(helo);
-BoxedBlock goodbye = new BoxedBlock(godbye);
- VComposition hellogoodbye = new VComposition(hello, goodbye);
-
- VComposition helogodbye = new VComposition(helo,godbye);
-
- BoxedBlock helogodbyeblock = new BoxedBlock(helogodbye);
- TBUtils.print(pen, helogodbyeblock);
- TBUtils.print(pen,hellogoodbye);
-
- HComposition comp1 = new HComposition(hello, godbye);
- HComposition comp2 = new HComposition(godbye,hello);
- TBUtils.print(pen,comp1);
- TBUtils.print(pen,comp2);
     // Clean up after ourselves.
     pen.close();
   } // main(String[])
 
 } // class TBExpt
+
+/*
+ * / Create a block to use
+    TextBlock block = new TextLine("This is a test.");
+
+    // Print out the block
+    TBUtils.print(pen, block);
+
+      TextBlock amazing = new TextLine("You are amazing.");
+
+      BoxedBlock awesome = new BoxedBlock(amazing);
+
+      TBUtils.print(pen, awesome);
+      
+      BoxedBlock incredible = new BoxedBlock(awesome);
+      TBUtils.print(pen, incredible);
+
+      TextBlock empty = new TextLine(" ");
+      BoxedBlock emptyBox = new BoxedBlock(empty);
+      TBUtils.print(pen, emptyBox);
+
+  TextBlock helo = new TextLine("hello");
+  TextBlock godbye = new TextLine("goodbye");
+  BoxedBlock hello = new BoxedBlock(helo);
+  BoxedBlock goodbye = new BoxedBlock(godbye);
+  VComposition hellogoodbye = new VComposition(hello, goodbye);
+
+  VComposition helogodbye = new VComposition(helo,godbye);
+
+  BoxedBlock helogodbyeblock = new BoxedBlock(helogodbye);
+  TBUtils.print(pen, helogodbyeblock);
+  TBUtils.print(pen,hellogoodbye);
+
+  HComposition comp1 = new HComposition(hello, godbye);
+  HComposition comp2 = new HComposition(godbye,hello);
+  TBUtils.print(pen,comp1);
+  TBUtils.print(pen,comp2);
+ */
